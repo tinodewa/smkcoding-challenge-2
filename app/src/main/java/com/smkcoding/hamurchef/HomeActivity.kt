c274109f10885618784dca57369e76f2c1da3a78
tinodewa <dtino0812@gmail.com>
2020-05-14 12:39:59 +0700

    Fixing tablayout

--- app/src/main/java/com/smkcoding/hamurchef/HomeActivity.kt
+++ app/src/main/java/com/smkcoding/hamurchef/HomeActivity.kt
@@ -11,17 +11,23 @@ import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
 
+    val menuIconOnRender = arrayOf(
+        R.drawable.ic_recipe_book_colored,
+        R.drawable.ic_search_food_black,
+        R.drawable.ic_profile_man_black
+    )
+
     val menuIconUnselected = arrayOf(
         R.drawable.ic_recipe_book_black,
         R.drawable.ic_search_food_black,
         R.drawable.ic_profile_man_black
     )
 
-//    val menuIconSelected = arrayOf(
-//        R.drawable.ic_recipe_book_colored,
-//        R.drawable.ic_search_food_colored,
-//        R.drawable.ic_profile_man_colored
-//    )
+    val menuIconSelected = arrayOf(
+        R.drawable.ic_recipe_book_colored,
+        R.drawable.ic_search_food_colored,
+        R.drawable.ic_profile_man_colored
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,8 +39,31 @@ class HomeActivity : AppCompatActivity() {
             TabConfigurationStrategy { tab, position ->
                 tab.icon = ResourcesCompat.getDrawable(
                     resources,
-                    menuIconUnselected[position], null
+                    menuIconOnRender[position], null
                 )
             }).attach()
+
+        home_tl.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                home_vp.currentItem = tab!!.position
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+                home_vp.currentItem = tab!!.position
+                tab.setIcon(ResourcesCompat.getDrawable(
+                    resources,
+                    menuIconUnselected[tab.position], null
+                ))
+            }
+
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                home_vp.currentItem = tab!!.position
+                tab.setIcon(ResourcesCompat.getDrawable(
+                    resources,
+                    menuIconSelected[tab.position], null
+                ))
+            }
+
+        })
     }
 }
